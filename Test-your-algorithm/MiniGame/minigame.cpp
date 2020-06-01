800a155cda7f4100f1946a7c6e9e716831cc36f2
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2020-06-01 19:14:11 +0900

    [MiniGame] create framework

---
+++ Test-your-algorithm/MiniGame/minigame.cpp
@@ -0,0 +1,55 @@
+#include "minigame.h"
+
+/*
+	game play tick
+*/
+void MiniGame::ProcessInputOutput()
+{
+	Packet packet;
+
+	// handle input
+	for (auto& userPacketQueue : inputPacket_)
+	{
+		const UserUID user = userPacketQueue.first;
+		PacketQueue& packetQueue = userPacketQueue.second;
+
+		if (packetQueue.try_pop(packet))
+			HandleUserInput(user, packet);
+	}
+
+	// play game
+	PlayGame();
+
+	// get output
+	for (const auto& user : userSet_)
+		if (UserStatusUpdate(user, packet))
+			outputPacket_[user].push(packet);
+}
+
+/*
+	push just one input packet for one user
+*/
+void MiniGame::PushUserInputPacket(const UserUID& user, const Packet& packet)
+{
+	inputPacket_[user].push(packet);
+}
+
+/*
+	Get just one output packet for each user
+*/
+const MiniGame::UserOutputPacket MiniGame::PopUserOutputPacket()
+{
+	UserOutputPacket outputPacket;
+
+	for (auto& userOutputPacket : outputPacket_)
+	{
+		const UserUID& user = userOutputPacket.first;
+		PacketQueue& packetQueue = userOutputPacket.second;
+
+		Packet packet;
+		if (packetQueue.try_pop(packet))
+			outputPacket[user] = packet;
+	}
+
+	return outputPacket;
+}
---
+++ Test-your-algorithm/MiniGame/minigame.h
@@ -0,0 +1,41 @@
+#pragma once
+#define _SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING
+#include "macro.h"
+#include "packet.h"
+#include <unordered_map>
+#include <concurrent_queue.h>
+#include <concurrent_unordered_map.h>
+#include <concurrent_unordered_set.h>
+
+/*
+	mini-game framework
+*/
+class MiniGame
+{
+public:
+	using UserUID = unsigned int;
+	using PacketQueue = concurrency::concurrent_queue<Packet>;
+	using UserPacketQueue = concurrency::concurrent_unordered_map<UserUID, PacketQueue>;
+	using UserOutputPacket = std::unordered_map<UserUID, Packet>;
+	using UserSet = concurrency::concurrent_unordered_set<UserUID>;
+
+public:
+	MiniGame() = default;
+	virtual ~MiniGame() = default;
+	
+	void InsertUser(const UserUID& user) { userSet_.insert(user); }
+	void ProcessInputOutput();
+	void PushUserInputPacket(const UserUID& user, const Packet& packet);
+	const UserOutputPacket PopUserOutputPacket();
+
+	virtual bool IsFinish() = 0;
+
+protected:
+	virtual void HandleUserInput(const UserUID& user, const Packet& packet) = 0;
+	virtual void PlayGame() = 0;
+	virtual bool UserStatusUpdate(const UserUID& user, const Packet& packet) = 0;
+
+	UserSet userSet_;
+	UserPacketQueue inputPacket_;
+	UserPacketQueue outputPacket_;
+};
